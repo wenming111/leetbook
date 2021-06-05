3bc75c35211d2f49182f0815662273157a05edf3
wenming <wenming.1995@qq.com>
2021-06-05 17:31:24 +0800

    add go set

--- slice_windows/3.go
+++ slice_windows/3.go
@@ -1,7 +1,8 @@
 // 基本滑动窗口题型
 // 遍历数组，每次添加最右的元素，并判断当前窗口内是否包含此元素
 // 如果包含 则一直右移窗口的左边界（一个while实现）
-// 最后判断结果
+
+// go中set的实现，可以把map的value设置成void类型，不占用内存。void类型可以用struct{}来定义
 type void struct{}
 func lengthOfLongestSubstring(s string) int {
     look_up := make(map[byte]void)
